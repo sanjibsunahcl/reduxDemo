d871dfb44c2ece232b4fbbe750bda4660b16d14b
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-03-22 10:09:15 +0530

    Button press set up

--- src/screen/itemEnzyme.js
+++ src/screen/itemEnzyme.js
@@ -3,16 +3,27 @@ import {Text, View, Button} from 'react-native';
 
 export const dataChange = x => x * 10;
 
-const ItemsEnzyme = () => {
+const ItemsEnzyme = ({onCountChange = () => {}}) => {
   const [status, setStatus] = useState('');
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    onCountChange(count);
+  }, []);
 
   return (
     <View
       style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
       testID="todo-item">
       <Text>Sanjib</Text>
+      <Text testID="countText">Count : {count}</Text>
       <Text style={{fontSize: 18, color: 'black'}}>Items</Text>
-      <Button testID="myButton" title="Click"></Button>
+      <Button
+        testID="myButton"
+        title="Click"
+        onPress={() => {
+          setCount(count + 3);
+        }}></Button>
     </View>
   );
 };
